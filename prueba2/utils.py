0a5143f3fb48ccbec179564a3b218049e815a6f7
FrankyCalcetero <drakom.1126@hotmail.com>
2022-10-11 22:34:44 -0500

    otroc

--- prueba2/utils.py
+++ prueba2/utils.py
@@ -23,12 +23,6 @@ REQ_FORGOT = 'REQ_FORGOT'
 U_UNCONFIRMED = 'UNCONFIRMED'
 U_CONFIRMED = 'CONFIRMED'
 
-def sql_connections():
-    try:
-        con = sqlite3.connect('EMPLEADOS_IANSA.db')
-        return con
-    except Error:
-        print(Error)
 
 
 def isEmailValid(email):
@@ -47,36 +41,3 @@ def isPasswordValid(password):
 	else:
 		return False
 
-<<<<<<< HEAD
-def sql_login_empleados(Correo, Contrasña):
-    strsql = "SELECT Categoria FROM Empleados WHERE Correo = '"+Correo+"' AND Contraseña = '"+Contrasña+"';"
-    con = sql_connections()
-    oCursor = con.cursor()
-    oCursor.execute(strsql)
-    Cate= oCursor.fetchone()
-    con.close()
-    return Cate
-
-def sql_select_empleados(Cedula):
-    strsql = "SELECT  Nombre, Cargo, Fecha_Ingreso, Fecha_Fin,Tipo_Contrato, Dependencia, Salario, Puntaje FROM Empleados WHERE Cedula = " + Cedula  + ";"
-    con = sql_connections()
-    oCursor = con.cursor()
-    oCursor.execute(strsql)
-    Empleado = oCursor.fetchall()
-    con.close()
-    return Empleado
-
-def sql_edit_empleados(Cedula, Nombre, Cargo, Fecha_Ingreso, Fecha_Fin,Tipo_Contrato, Dependencia, Salario, Puntaje, Correo,Contrasña):
-    strsql = "UPDATE Empleados SET Cedula = '" + Cedula + "',  Nombre ='" + Nombre + "', Cargo = '" + Cargo + "', Fecha_Ingreso = " + Fecha_Ingreso + ", Fecha_Fin = " + Fecha_Fin + " , Tipo_Contrato = '" + Tipo_Contrato + "' , Dependencia ='" +  Dependencia + "', Salario = " +  Salario + ", Puntaje = " + Puntaje + ", Correo = '" + Correo+ "', Contrasña = '" + Contrasña +"' WHERE Cedula = " + Cedula + ";"
-    con = sql_connections()
-    oCursor = con.cursor()
-    oCursor.execute(strsql)
-    con.commit()
-    con.close()
-=======
-
-
-
->>>>>>> Franky_Backend
-    
-    
