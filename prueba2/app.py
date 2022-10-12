246f5cc4d4b0b471ff262c01453a7ef6122ae9f8
FrankyCalcetero <drakom.1126@hotmail.com>
2022-10-11 22:35:38 -0500

    app

--- prueba2/app.py
+++ prueba2/app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, jsonify, render_template, flash, request, redirect, url_for
+import email
+from flask import Flask, render_template, flash, request, redirect, url_for
 import utils
 from utils import *
 
@@ -31,6 +32,43 @@ def login():
     else:
         return render_template('login.html')
 
+
+def sql_login_empleados(Correo, Contrasña):
+    strsql = "SELECT Categoria FROM Empleados WHERE Correo = '"+Correo+"' AND Contraseña = '"+Contrasña+"';"
+    con = get_db()
+    oCursor = con.cursor()
+    oCursor.execute(strsql)
+    Cate= oCursor.fetchone()
+    close_db()
+    return Cate
+
+
+def sql_select_empleados(Cedula):
+    strsql = "SELECT  Nombre, Cargo, Fecha_Ingreso, Fecha_Fin,Tipo_Contrato, Dependencia, Salario, Puntaje FROM Empleados WHERE Cedula = " + Cedula  + ";"
+    con = get_db
+    oCursor = con.cursor()
+    oCursor.execute(strsql)
+    Empleado = oCursor.fetchall()
+    close_db()
+    return Empleado
+
+def sql_edit_empleados(Cedula, Nombre, Cargo, Fecha_Ingreso, Fecha_Fin,Tipo_Contrato, Dependencia, Salario, Puntaje, Correo,Contrasña):
+    strsql = "UPDATE Empleados SET Cedula = '" + Cedula + "',  Nombre ='" + Nombre + "', Cargo = '" + Cargo + "', Fecha_Ingreso = " + Fecha_Ingreso + ", Fecha_Fin = " + Fecha_Fin + " , Tipo_Contrato = '" + Tipo_Contrato + "' , Dependencia ='" +  Dependencia + "', Salario = " +  Salario + ", Puntaje = " + Puntaje + ", Correo = '" + Correo+ "', Contrasña = '" + Contrasña +"' WHERE Cedula = " + Cedula + ";"
+    con = get_db()
+    oCursor = con.cursor()
+    oCursor.execute(strsql)
+    con.commit()
+    close_db()
+
+
+
+
+
+
+
+
+
+
 @app.route('/register/')
 def register():
     return render_template('register.html')
@@ -58,19 +96,5 @@ def administrador():
 @app.route('/usuarioFinal/')
 def usuarioFinal():
     return render_template('usuarioFinal.html')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
